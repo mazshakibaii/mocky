882669ed65318794d6468b527161c362d8f0097e
mazshakibaii <mazshakibaii@gmail.com>
2025-04-30 17:10:01 +0000

    fix: removed zod import from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,8 @@
-import { z } from "zod";
 import {
   createMocky,
   type GenerateOptions,
   type MockyOptions,
 } from "./generator/Mocky";
-
-const mocky = createMocky({
-  schema: z.object({
-    name: z.string(),
-    age: z.number(),
-  }),
-});
 
 export { createMocky };
 export type { MockyOptions, GenerateOptions };
